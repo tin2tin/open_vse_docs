719a8bfb656aa6a67f94c3994c15ceac0366a7bc
tin2tin <tintwotin@gmail.com>
2021-12-05 15:27:01 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,49 @@
+bl_info = {
+    "name": "Open VSE Documentation",
+    "author": "tintwotin",
+    "version": (1, 0),
+    "blender": (3, 00, 0),
+    "location": "Sequencer > View Menu > Documentation",
+    "description": "Expose option to ppen online VSE documentation",
+    "warning": "",
+    "doc_url": "",
+    "category": "Seqeuncer",
+}
+
+import bpy
+from bpy.types import Operator
+import webbrowser
+
+
+class OPERATOR_OT_open_vse_docs(Operator):
+    """Open online VSE documentation"""
+
+    bl_idname = "sequencer.vse_docs"
+    bl_label = "Documentation"
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+
+        webbrowser.open("vse-docs.readthedocs.io")
+
+        return {"FINISHED"}
+
+
+def menu_append(self, context):
+    layout = self.layout
+    layout.separator()
+    layout.operator(OPERATOR_OT_open_vse_docs.bl_idname)
+
+
+def register():
+    bpy.utils.register_class(OPERATOR_OT_open_vse_docs)
+    bpy.types.SEQUENCER_MT_view.append(menu_append)
+
+
+def unregister():
+    bpy.utils.unregister_class(OPERATOR_OT_open_vse_docs)
+    bpy.types.SEQUENCER_MT_view.remove(menu_append)
+
+
+if __name__ == "__main__":
+    register()
